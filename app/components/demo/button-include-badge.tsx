1bf8b26df03dd51b5d14058793604eb55f436e83
timlee <hungtington.dev@gmail.com>
2025-04-04 20:35:18 -0400

    🧹 chore: try make a regression

--- app/components/demo/button-include-badge.tsx
+++ app/components/demo/button-include-badge.tsx
@@ -2,7 +2,7 @@ import { Cta } from "#/components/ui/Cta/Cta.tsx";
 
 export default () => (
   <Cta variant="outline" size='sm' className="tw:shadow-none">
-    MCP Server!!!!!!!!!!!!!
+    MCP Server 🔥
     {''}
     <Cta asChild muted shapes={['badge']} variant="outline" size="sm" className='tw:rounded-full'><span>Beta</span></Cta>
   </Cta>
